a38f1820846f9256712460a73b764795018ae271
Mike <mdebree@winbase.nl>
2018-02-20 19:28:30 +0100

    Eerste opzet
    DONE:
    - Core, android en iOS projecten opgezet
    - MvvMCross compatible gemaakt

--- SnackPlanning.Core/App.cs
+++ SnackPlanning.Core/App.cs
@@ -11,7 +11,7 @@ namespace SnackPlanning.Core
                 .AsInterfaces()
                 .RegisterAsLazySingleton();
 
-            RegisterAppStart<ViewModels.MainViewModel>();
+            RegisterNavigationServiceAppStart<ViewModels.MainViewModel>();
         }
     }
 }
